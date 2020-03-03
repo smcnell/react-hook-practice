8e55141f6019d4e32731527314943241a6e14184
Shannon McNellis <shannonmcnellis@Shannons-MBP.hsd1.sc.comcast.net>
2020-03-03 15:12:25 -0500

    initial commit

---
+++ src/AppFunction.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    const incrementCount = () => {
+        setCount(prevCount => prevCount + 1)
+
+    };
+
+    return (
+        <button onClick={incrementCount}>
+            I was clicked {count} times
+        </button>
+    )
+};
+
+export default App;
