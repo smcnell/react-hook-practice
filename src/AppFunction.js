5b5cb8f20513bf55d04123391844dc6642615e07
Shannon McNellis <shannonmcnellis@Shannons-MBP.hsd1.sc.comcast.net>
2020-03-04 13:18:16 -0500

    added toggle light feature

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,16 +2,34 @@ import React, {useState} from "react";
 
 const App = () => {
     const [count, setCount] = useState(0);
+    const [isOn, setIsOn] = useState(false);
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1)
+    };
 
+    const toggleLight = () => {
+        setIsOn(prevIsOn => !prevIsOn)
     };
 
     return (
-        <button onClick={incrementCount}>
-            I was clicked {count} times
-        </button>
+        <>
+            <h2>Counter</h2>
+            <button onClick={incrementCount}>
+                I was clicked {count} times
+            </button>
+
+            <h2>Toggle Light</h2>
+            <div
+            style={{
+                height: '50px',
+                width: '50px',
+                background: isOn ? 'yellow' : 'grey'
+            }}
+            onClick={toggleLight}
+            />
+
+        </>
     )
 };
 
