2f48318780e1b25d33327eb62a9efdd6a6bab601
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2020-03-09 19:32:53 -0400

    use effec added

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,8 +1,13 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const App = () => {
     const [count, setCount] = useState(0);
     const [isOn, setIsOn] = useState(false);
+
+    //effect function is called after every re-render (or after a state change that causes a re-render)
+    useEffect(() => {
+        document.title = `You have clicked ${count} times`;
+    });
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1)
@@ -20,12 +25,16 @@ const App = () => {
             </button>
 
             <h2>Toggle Light</h2>
-            <div
-            style={{
-                height: '50px',
-                width: '50px',
-                background: isOn ? 'yellow' : 'grey'
+            <img
+                src={
+                    isOn ? 'https://icon.now.sh/highlight/fd0' : 'https://icon.now.sh/highlight/aaa'
+                }
+                style={{
+                    height: '50px',
+                    width: '50px',
+                    background: isOn ? 'yellow' : 'grey'
             }}
+            alt="Lightbulb"
             onClick={toggleLight}
             />
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppClass from './AppClass';
 import App from './AppFunction';
 import * as serviceWorker from './serviceWorker';
 
