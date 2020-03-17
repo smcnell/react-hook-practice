e5fdcf9302d219b1df082c2d9738bad17d9429b4
Shannon McNellis <shannonmcnellis@Shannons-MBP.hsd1.sc.comcast.net>
2020-03-17 15:11:20 -0400

    added mouse position

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -3,11 +3,28 @@ import React, {useState, useEffect} from "react";
 const App = () => {
     const [count, setCount] = useState(0);
     const [isOn, setIsOn] = useState(false);
+    const [mousePosition, setMousePosition] = useState({x: null, y: null})
 
     //effect function is called after every re-render (or after a state change that causes a re-render)
     useEffect(() => {
         document.title = `You have clicked ${count} times`;
-    });
+        window.addEventListener('mousemove', handleMouseMove);
+
+        //this cleanup function goes at the end of Use Effect function... this is performed when component unmounts or before effect runs
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove)
+        }
+    },
+        //this is if we want to prevent the useEffect from running on every render (only if the value changes as specified below will the side effect be run again
+        [count]
+    );
+
+    const handleMouseMove = event => {
+        setMousePosition({
+            x: event.pageX,
+            y: event.pageY
+        })
+    };
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1)
@@ -37,6 +54,10 @@ const App = () => {
             alt="Lightbulb"
             onClick={toggleLight}
             />
+
+            <h2>Current Mouse Position</h2>
+            {JSON.stringify(mousePosition, null, 2)}
+            <br />
 
         </>
     )
