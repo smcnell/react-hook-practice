10f418bed6747d5362c596beb9a1219fd26ddfd0
Shannon McNellis <shannonmcnellis@Shannons-MBP.hsd1.sc.comcast.net>
2020-03-19 17:26:26 -0400

    register page additions

--- src/Register.js
+++ src/Register.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Register() {
+
+    const initialFormState = {
+        username: "",
+        password: "",
+        email: ""
+    };
+
+    const [form, setForm] = useState(initialFormState);
+
+    const [user, setUser] = useState(null);
+
+    const handleChange = event => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+
+        })
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setUser(form)
+        setForm(initialFormState);
+    };
+
     return (
-    <div>
-        Register
-    </div>
-    )
+        <div
+            style={{
+                textAlign: "center"
+            }}
+        >
+            <h2>Register</h2>
+            <form
+                style={{
+                    display: 'grid',
+                    alignItems: 'center',
+                    justifyItems: 'center'
+                }}
+                onSubmit={handleSubmit}
+            >
+                <input type="text"
+                       placeholder="Username"
+                       name="username"
+                       onChange={handleChange}
+                       value={form.username}
+                />
+                <input type="password"
+                       placeholder="Password"
+                       name="password"
+                       onChange={handleChange}
+                       value={form.password}
+                />
+                <input type="email"
+                       placeholder="Email address"
+                       name="email"
+                       onChange={handleChange}
+                       value={form.email}
+
+
+                />
+                <button type="submit">Submit</button>
+
+            </form>
+
+            {user && JSON.stringify(user, null, 2)}
+        </div>
+    );
 
 }
