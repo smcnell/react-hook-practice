b70a997b4d8601ef51beba44c5466a1e4874d35e
Shannon McNellis <shannonmcnellis@Shannons-MBP.hsd1.sc.comcast.net>
2020-03-19 16:06:23 -0400

    added login and register page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './AppFunction';
+import App from './App';
+import Login from './Login';
+import Register from './Register';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Register/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
